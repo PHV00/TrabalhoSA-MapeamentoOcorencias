19252eff1923d4a1c67add25442e7480c5a6d7a9
Pedro Henrique Vitoreti <133826934+PHV00@users.noreply.github.com>
2023-11-30 17:24:53 -0300

    versão sla

--- Programa/Mapeamento_Ocorrencias/src/UsuarioCrud.java
+++ Programa/Mapeamento_Ocorrencias/src/UsuarioCrud.java
@@ -1,36 +1,75 @@
 public class UsuarioCrud {
     
     BancoDeDados bd;
+    Validacao vl;
+    //usuario comum
     private int cpf;
     private String nome;
     private int telefone;
-    private int senha; 
-
-    public UsuarioCrud(BancoDeDados bd)
+    private int senha;
+    //usuario admim 
+    private boolean admin;//n sei se precisa aqui
+    //logado
+    private boolean logado;// n sei se precisa aqui
+    
+    public UsuarioCrud(BancoDeDados bd, Validacao vl)
     {
         this.bd=bd;
-    }
-
-    public void cadastrarUsuario()
-    {
-        cpf=EntradaSaida.inserirValorInt("Digite seu cpf:");
-        telefone=EntradaSaida.inserirValorInt("Digite seu telefone:");
-        nome=EntradaSaida.inserirValorString("Digite seu nome completo:");
-        senha=EntradaSaida.inserirValorInt("Digite sua senha:"); 
-        Usuario usuario= new Usuario(cpf, nome, telefone, senha);
-        bd.adicionarUsuario(usuario);
-    }
-
-    public void excluirUsuario()
-    {
-        if(EntradaSaida.inserirValorString("Confirmar? S/N").toUpperCase()=="S")
-        {
-            
-        }
+        this.vl=vl;
     }
 
     public void executarCrud() {
-        EntradaSaida.mostrarMsg("1-Cadastrar Usuario");
+        String menu="";
+        menu+="1- Cadatrar Usuario\n2-Excluir Usuarios\n3-Emitir Lista de usuarios";
+        int opcao=EntradaSaida.inserirValorInt(menu);
     }
 
+    public void cadastrarUsuario()//é necessario validar todos os dados?========================================================
+    {
+        this.cpf=EntradaSaida.inserirValorInt("Digite seu cpf:");
+        this.telefone=EntradaSaida.inserirValorInt("\nDigite seu telefone:");
+        this.nome=EntradaSaida.inserirValorString("\nDigite seu nome completo:");
+        this.senha=EntradaSaida.inserirValorInt("\nDigite sua senha:");
+        
+        Usuario usuario= new Usuario(this.cpf, this.nome, this.telefone, this.senha);
+        bd.inserirUsuario(usuario);
+        
+        if(EntradaSaida.inserirValorString("\nDeseja ser admim? S/N:").equals("S"))this.cadastrarAdmin(usuario);//validar talvez com switch
+    }
+    public void cadastrarAdmin(Usuario usuario)// não sei se funciona  
+    {
+        Usuario adm=this.bd.encontrarUsuario("adm");//
+        int i=0;
+        boolean senhaCorreta;
+        do{
+            int senha=EntradaSaida.inserirValorInt("\nInforme a senha Adm:");
+            senhaCorreta=vl.validarAdm(senha);
+            if(senhaCorreta==true)
+            { 
+                usuario.setAdmin(true);
+                break;
+            }
+            else EntradaSaida.mostrarMsg("\nSenha Invalida!!! "+(i+1)+"º tentativas de 3");
+            i++;
+        }while(senhaCorreta==false && i<3);//rever a logica
+    }
+
+    public void deletarUsuario()//rever logica e inclementar no banco de dados
+    {
+        Usuario usuario;
+        usuario=this.bd.encontrarUsuario(EntradaSaida.inserirValorString("Digite o nome:"));
+        if(usuario.getNome().equals("ERROR")) EntradaSaida.mostrarMsg("Usuario não encontrado!");
+        else{
+            if(EntradaSaida.inserirValorString("Confirmar? S/N").toUpperCase().equals("S")) this.bd.excluirUsuario(usuario);
+        }
+    }
+
+    public void emitirLista()
+    {   
+        //dá pra fazer sem o foreach, usando um contador e mudadno no banco de dados a função visualizarusuario e mudar de 
+        //recber usuario par receber um contador da posição e aqui colocar um for q passa por toda lista sla
+        for (Usuario usuario  : this.bd.getListaUsuarios()) {
+            
+        }
+    }
 }
